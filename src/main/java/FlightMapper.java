34c4cfa504a004e2ac307b77f52f96df7cda5b6a
BokarevDmitry <dbokarevv@gmail.com>
2019-10-10 01:58:01 +0300

    Scripted auto-commit on change (2019-10-10 01:58:01) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -9,6 +9,8 @@ public class FlightMapper extends Mapper<Object, Text, Text, Text> {
     @Override
     protected void map(Object key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] pieces = value.toString().split(",");
-        context.write(new Text(pieces[14]), new Text("delayTime " + pieces[18]));
+        if (pieces[18].length() > 0) {
+            context.write(new Text(pieces[14]), new Text("delayTime " + pieces[18]));
+        }
     }
 }
