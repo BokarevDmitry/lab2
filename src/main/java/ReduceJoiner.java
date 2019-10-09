00239801393f11b41dcd2dab722a64526c20e8d6
BokarevDmitry <dbokarevv@gmail.com>
2019-10-10 02:24:00 +0300

    Scripted auto-commit on change (2019-10-10 02:24:00) by gitwatch.sh

--- src/main/java/ReduceJoiner.java
+++ src/main/java/ReduceJoiner.java
@@ -20,6 +20,6 @@ public class ReduceJoiner extends Reducer<Text, Text, Text, Text> {
                     timeDelay = pieces[1];
             }
         }
-        context.write(new Text(airportName), new Text(timeDelay));
+        context.write(key, new Text(timeDelay));
     }
 }
