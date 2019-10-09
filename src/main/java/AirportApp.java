6af78adce8f9134f06d869de2c8d8d4e5ddba46e
BokarevDmitry <dbokarevv@gmail.com>
2019-10-10 02:03:12 +0300

    Scripted auto-commit on change (2019-10-10 02:03:12) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -1,7 +1,7 @@
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import java.nio
+import java.nio.
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
