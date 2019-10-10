3d6f2eba6f72ea0e44bc1caa040a7cb3ec05a27f
BokarevDmitry <dbokarevv@gmail.com>
2019-10-10 03:06:01 +0300

    Scripted auto-commit on change (2019-10-10 03:06:01) by gitwatch.sh

--- src/main/java/ReduceJoiner.java
+++ src/main/java/ReduceJoiner.java
@@ -10,20 +10,32 @@ public class ReduceJoiner extends Reducer<Text, Text, Text, Text> {
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         String airportName = "";
-        String timeDelay = "";
+        String delayLine = "";
+
+        int count = 0;
         Double time = 0.00;
+        Double minTime = 0.00;
+        Double maxTime = 0.00;
+
+
         for (Text value:values) {
             String pieces[] = value.toString().split(";");
             if (pieces[0].equals("airportName")) {
                 airportName = pieces[1];
             }
             else if (pieces[0].equals("delayTime")) {
-                    timeDelay = pieces[1];
-                    time += Float.parseFloat(pieces[1]);
+                    //timeDelay = pieces[1];
+                    count++;
+                    double timeDelay = Float.parseFloat(pieces[1]);
+
+                    time += timeDelay;
+                    if (timeDelay<minTime) minTime=timeDelay;
+                    if (timeDelay>maxTime) maxTime=timeDelay;
             }
         }
         if (time>0) {
-            context.write(new Text(airportName), new Text(time.toString()));
+            delayLine = "Total DelayTime = " + time + " Min = " + minTime + " Max = " + maxTime;
+            context.write(new Text(airportName), new Text(delayLine));
         }
     }
 }
