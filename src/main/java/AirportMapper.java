8ea8811d2d5dd523d54c48df3f9ce3f4996d22c5
BokarevDmitry <dbokarevv@gmail.com>
2019-10-12 12:11:45 +0300

    Scripted auto-commit on change (2019-10-12 12:11:45) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -5,9 +5,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class AirportMapper extends Mapper<Object, Text, TextPair, Text> {
+public class AirportMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
-    protected void map(Object key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] pieces = value.toString().split("\",\"");
         if (!pieces[0].equals("Code,Description")) {
             context.write(new TextPair(pieces[0].substring(1), "1"), new Text(pieces[1]));
--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -1,11 +1,12 @@
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class FlightMapper extends Mapper<TextPair, Text, TextPair, Text> {
+public class FlightMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
-    protected void map(TextPair key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] pieces = value.toString().split(",");
         if (!pieces[18].equals("\"ARR_DELAY_NEW\"")) {
             if (pieces[18].length()>0 && Float.parseFloat(pieces[18])>0) {
