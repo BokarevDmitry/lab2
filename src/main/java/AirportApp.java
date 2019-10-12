7cc93ea47d493940c56aadf39b97a7779c44ae95
BokarevDmitry <dbokarevv@gmail.com>
2019-10-12 10:40:10 +0300

    Scripted auto-commit on change (2019-10-12 10:40:10) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -25,16 +25,17 @@ public class AirportApp {
         Job job = Job.getInstance();
         job.setJarByClass(AirportApp.class);
         job.setJobName("AirportApp");
-
-        job.setReducerClass(ReduceJoiner.class);
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightMapper.class);
+
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setReducerClass(ReduceJoiner.class);
+
 
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        job.setNumReduceTasks(2);
+        job.setNumReduceTasks(1);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
