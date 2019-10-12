4bca4dfc289814c3ab06afbfc1d3d8934533ac5a
BokarevDmitry <dbokarevv@gmail.com>
2019-10-12 11:06:35 +0300

    Scripted auto-commit on change (2019-10-12 11:06:35) by gitwatch.sh

--- src/main/java/ReduceJoiner.java
+++ src/main/java/ReduceJoiner.java
@@ -1,41 +1,30 @@
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
-
 import org.apache.hadoop.io.Text;
 import java.io.IOException;
 import java.util.Iterator;
 
-public class ReduceJoiner extends Reducer<Text, Text, Text, Text> {
+public class ReduceJoiner extends Reducer<TextPair, Text, Text, Text> {
     @Override
-    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        String airportName = "";
-        String delayLine = "";
-
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         int count = 0;
         Double time = 0.00;
         Double minTime = 9999.00;
         Double maxTime = 0.00;
 
+        Iterator<Text> iter = values.iterator();
+        Text airportName = new Text(iter.next());
+        while (iter.hasNext()) {
+            String timeDelayInfo = iter.next().toString();
+            double timeDelay = Float.parseFloat(timeDelayInfo);
 
-        for (Text value:values) {
-            String pieces[] = value.toString().split(";");
-            if (pieces[0].equals("airportName")) {
-                airportName = pieces[1];
-            }
-            else if (pieces[0].equals("delayTime")) {
-                    //timeDelay = pieces[1];
-                    count++;
-                    double timeDelay = Float.parseFloat(pieces[1]);
-
-                    time += timeDelay;
-                    if (timeDelay<minTime) minTime=timeDelay;
-                    if (timeDelay>maxTime) maxTime=timeDelay;
-            }
+            count++;
+            time += timeDelay;
+            if (timeDelay<minTime) minTime=timeDelay;
+            if (timeDelay>maxTime) maxTime=timeDelay;
         }
-        if (time>0) {
-            delayLine = "Total DelayTime = " + time + " Average = " + time/count + " Min = " + minTime + " Max = " + maxTime;
-            context.write(new Text(airportName), new Text(delayLine));
+        if (count>0) {
+            String delayLine = "Total DelayTime = " + time + " Average = " + time/count + " Min = " + minTime + " Max = " + maxTime;
+            context.write(key.getFirst(), new Text(delayLine));
         }
     }
 }
