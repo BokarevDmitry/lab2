2349220df2741b9877f45f0059de546f9809c4c4
BokarevDmitry <dbokarevv@gmail.com>
2019-10-12 14:04:01 +0300

    Scripted auto-commit on change (2019-10-12 14:04:01) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class AirportMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
-        String[] pieces = CSVParser.parseCSV(value);
+        String[] pieces = CSVParser.parseAirports(value);
         if (!pieces[0].equals("Code")) {
             context.write(new TextPair(CSVParser.getAirportName(pieces[0]), "0"), new Text(CSVParser.getAirportName(pieces[1])));
         }
--- src/main/java/CSVParser.java
+++ src/main/java/CSVParser.java
@@ -4,6 +4,9 @@ public class CSVParser {
     public static String[] parseCSV (Text s) {
         return s.toString().split(",");
     }
+    public static String[] parseAirports (Text s) {
+        return s.toString().split("\",\"");
+    }
 
     public static String getAirportName (String s) {
         return s.replaceAll("[\"]","");
