7448802de52855735ad0a4c10c9b40546357bf45
BokarevDmitry <dbokarevv@gmail.com>
2019-10-12 12:10:43 +0300

    Scripted auto-commit on change (2019-10-12 12:10:43) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -3,9 +3,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class FlightMapper extends Mapper<Object, Text, TextPair, Text> {
+public class FlightMapper extends Mapper<TextPair, Text, TextPair, Text> {
     @Override
-    protected void map(Object key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+    protected void map(TextPair key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] pieces = value.toString().split(",");
         if (!pieces[18].equals("\"ARR_DELAY_NEW\"")) {
             if (pieces[18].length()>0 && Float.parseFloat(pieces[18])>0) {
