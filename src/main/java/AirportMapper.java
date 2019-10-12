6a3bd1053cb2e1e96d5ccf3f492d3b3aa908251a
BokarevDmitry <dbokarevv@gmail.com>
2019-10-12 10:52:01 +0300

    Scripted auto-commit on change (2019-10-12 10:52:01) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -10,7 +10,7 @@ public class AirportMapper extends Mapper<Object, Text, Text, Text> {
     protected void map(Object key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] pieces = value.toString().split("\",\"");
         if (!pieces[0].equals("Code,Description")) {
-            context.write(new TextPair(pieces[0].substring(1), "1"), new Text("airportName;" + pieces[1]));
+            context.write(new TextPair(pieces[0].substring(1), "1"), new Text(pieces[1]));
         }
         else {
             context.write(new Text("code"), new Text("airportName;descrip"));
