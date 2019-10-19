b1b2a750f0c7de9b233aa467ea48ac1e3e319d4a
BokarevDmitry <dbokarevv@gmail.com>
2019-10-19 11:17:56 +0300

    Scripted auto-commit on change (2019-10-19 11:17:56) by gitwatch.sh

--- src/main/java/bokarev/TextPair.java
+++ src/main/java/bokarev/TextPair.java
@@ -11,24 +11,23 @@ import org.apache.hadoop.io.WritableComparable;
 
 public class TextPair implements WritableComparable<TextPair>{
     public  Text code;
-    public  Text flag;
+    public  Text second;
 
-    public TextPair(String code, String flag){
-        this.code =new Text(code);
-        this.flag =new Text(flag);
-    }
-
-    public TextPair(Text code, Text flag){
+    public TextPair(Text code, Text second) {
         this.code = code;
-        this.flag = flag;
+        this.second = second;
+    }
+    public TextPair(String code, String second){
+        this.code =new Text(code);
+        this.second=new Text(second);
     }
 
     public Text getCode() {
         return code;
     }
 
-    public Text getFlag() {
-        return flag;
+    public Text getSecond() {
+        return second;
     }
 
 
@@ -38,29 +37,29 @@ public class TextPair implements WritableComparable<TextPair>{
         if (o == null || getClass() != o.getClass()) return false;
         TextPair textPair = (TextPair) o;
         return Objects.equals(code, textPair.code) &&
-                Objects.equals(flag, textPair.flag);
+                Objects.equals(second, textPair.second);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(code, flag);
+        return Objects.hash(code, second);
     }
 
     @Override
     public String toString() {
-        return code +"\t"+ flag;
+        return code +"\t"+second;
     }
 
     @Override
     public void readFields(DataInput in) throws IOException {
         code.readFields(in);
-        flag.readFields(in);
+        second.readFields(in);
     }
 
     @Override
     public void write(DataOutput out) throws IOException {
         code.write(out);
-        flag.write(out);
+        second.write(out);
     }
 
 
@@ -69,7 +68,7 @@ public class TextPair implements WritableComparable<TextPair>{
         int cmp= code.compareTo(tp.getCode());
         if(cmp!=0)
             return cmp;
-        return flag.compareTo(tp.getFlag());
+        return second.compareTo(tp.getSecond());
 
     }
 }
