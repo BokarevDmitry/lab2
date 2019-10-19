2c7e6f8b0363bda6a63b3cf305bc729c1435f9ad
BokarevDmitry <dbokarevv@gmail.com>
2019-10-19 11:49:54 +0300

    Scripted auto-commit on change (2019-10-19 11:49:54) by gitwatch.sh

--- src/main/java/bokarev/AirportMapper.java
+++ src/main/java/bokarev/AirportMapper.java
@@ -7,13 +7,15 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    private static String DESCRIPTION_LINE = "Code";
+
     @Override
     protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
-        String[] pieces = CSVParser.parseAirports(value);
-        if (!CSVParser.getAirCode(pieces).contains("Code")) {
-            String airportName = CSVParser.getAirportName(pieces);
-            String airportCode = CSVParser.getAirCode(pieces);
+        String[] columns = CSVParser.parseAirports(value);
+        String airportCode = CSVParser.getAirCode(columns);
+        String airportName = CSVParser.getAirportName(columns);
 
+        if (!airportCode.contains(DESCRIPTION_LINE)) {
             context.write(new TextPair(CSVParser.removeQuotes(airportCode), "0"), new Text(CSVParser.removeQuotes(airportName)));
         }
     }
--- src/main/java/bokarev/FlightMapper.java
+++ src/main/java/bokarev/FlightMapper.java
@@ -6,13 +6,15 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    private static String DESCRIPTION_LINE = "ARR_DELAY_NEW";
+
     @Override
     protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
-        String[] pieces = CSVParser.parseFlights(value);
-        if (!CSVParser.getDelayTime(pieces).equals("\"ARR_DELAY_NEW\"")) {
-            if (CSVParser.getDelayTime(pieces).length()>0 && Float.parseFloat(CSVParser.getDelayTime(pieces))>0) {
-                context.write(new TextPair(CSVParser.getAirportCode(pieces), "1"), new Text(CSVParser.getDelayTime(pieces)));
-            }
+        String[] columns = CSVParser.parseFlights(value);
+        String delayTime = CSVParser.getDelayTime(columns);
+
+        if (!delayTime.contains(DESCRIPTION_LINE) && delayTime.length()>0 && Float.parseFloat(delayTime)>0) {
+            context.write(new TextPair(CSVParser.getAirportCode(columns), "1"), new Text(delayTime));
         }
     }
 }
