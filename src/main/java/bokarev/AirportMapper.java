1f51134b1994493eab7023a4407e993cb5928047
BokarevDmitry <dbokarevv@gmail.com>
2019-10-19 11:15:57 +0300

    Scripted auto-commit on change (2019-10-19 11:15:57) by gitwatch.sh

--- src/main/java/bokarev/AirportMapper.java
+++ src/main/java/bokarev/AirportMapper.java
@@ -10,8 +10,11 @@ public class AirportMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] pieces = CSVParser.parseAirports(value);
-        if (!CSVParser.getAirportName(pieces).contains("Code")) {
-            context.write(new TextPair(CSVParser.getAirCode(pieces), "0"), new Text(CSVParser.getAirportName(pieces)));
+        String airportName = CSVParser.getAirportName(pieces);
+        airportName = CSVParser.removeQuotes(airportName);
+
+        if (!airportName.contains("Code")) {
+            context.write(new TextPair(CSVParser.getAirCode(pieces), "0"), new Text(airportName));
         }
     }
 }
--- src/main/java/bokarev/CSVParser.java
+++ src/main/java/bokarev/CSVParser.java
@@ -9,11 +9,13 @@ public class CSVParser {
         return s.toString().split("\",\"");
     }
 
+    public static String removeQuotes (String s) {return s.replaceAll("[\"]", "");}
+
     public static String getAirCode (String[] s) {return s[0];}
     public static String getAirportName (String[] s) {
-        return s[1].replaceAll("[\"]","");
+        return s[1];
     }
 
-    public static String getAirportCode (String[] s) {return s[18];}
-    public static String getDelayTime (String[] s) {return s[14];}
+    public static String getAirportCode (String[] s) {return s[14];}
+    public static String getDelayTime (String[] s) {return s[18];}
 }
