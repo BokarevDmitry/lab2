738a4a548aa02754f6889aaf5e5015ea32bbb1c5
BokarevDmitry <dbokarevv@gmail.com>
2019-10-19 11:31:38 +0300

    Scripted auto-commit on change (2019-10-19 11:31:38) by gitwatch.sh

--- src/main/java/bokarev/AirportMapper.java
+++ src/main/java/bokarev/AirportMapper.java
@@ -10,7 +10,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] pieces = CSVParser.parseAirports(value);
-        if (!pieces[0].contains("Code")) {
+        if (!CSVParser.getAirCode(pieces).contains("Code")) {
             String airportName = CSVParser.getAirportName(pieces);
             airportName = CSVParser.removeQuotes(airportName);
             context.write(new TextPair(CSVParser.getAirCode(pieces), "0"), new Text(airportName));
